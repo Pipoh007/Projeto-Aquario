3668a156bf4d438e382e01d95e7107a9a299845a
Pedro Henrique Rodriguez Daniel <69942411+Pipoh007@users.noreply.github.com>
2022-05-11 18:57:16 -0300

    Arquivos exemplo
    Bibliotecas ssd1306 e ds1307
    Códigos teste RTC e display OLED

--- Testes/teste_RTC.py
+++ Testes/teste_RTC.py
@@ -1,17 +1,27 @@
+# https://www.iotstarters.com/diy-digital-clock-with-rtc-ds1307-and-raspberry-pi-pico/
 from machine import I2C, Pin
 from ds1307 import DS1307
-from pico_i2c_lcd import I2cLcd
+#from pico_i2c_lcd import I2cLcd
 import utime
+from machine import Pin, SPI  
+from ssd1306 import SSD1306_SPI  
+import framebuf  
+from time import sleep
+from utime import sleep_ms
 
-i2c_lcd = I2C(id=1,scl=Pin(3),sda=Pin(2),freq=100000) # configuring and initializing the LCD display using I2C protocol.
-lcd = I2cLcd(i2c_lcd, 0x27, 2, 16)
+#i2c_lcd = I2C(id=1,scl=Pin(5),sda=Pin(4),freq=100000) # configuring and initializing the LCD display using I2C protocol.
+#lcd = I2cLcd(i2c_lcd, 0x27, 2, 16)
+
+i2c_SPI = SPI(0, 100000, mosi=Pin(19), sck=Pin(18))
+oled = SSD1306_SPI(128, 64, i2c_SPI, Pin(17),Pin(20), Pin(16))
 
 i2c_rtc = I2C(0,scl = Pin(1),sda = Pin(0),freq = 100000)  # We are also connecting the RTC module using I2C interface and hence we be using below
 #line of code to configure it.
 
+
 #Below code should be uncommented only to set the time for first time in RTC and then it should be commented back again.
 
-'''
+rtc = machine.RTC()
 year = int(input("Year : "))
 month = int(input("month (Jan --> 1 , Dec --> 12): "))
 date = int(input("date : "))
@@ -22,5 +32,12 @@ second = int(input("second : "))
 now = (year,month,date,day,hour,minute,second,0)
 rtc.datetime(now)
 print(rtc.datetime())
-'''
-
+RTC = ' '.join(str(e) for e in rtc.datetime())
+               
+oled.fill(0)
+oled.show()
+oled.contrast(255)
+sleep(1)
+oled.text(RTC,10,50,100)
+oled.show()
+sleep_ms(10)
